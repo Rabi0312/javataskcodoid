c7dc579f941ef73e01161501b659f9b0eb869886
Naresh <Naresh@Naresh23>
2023-04-14 15:54:11 +0530

    String to Date

---
+++ Project/src/org/task/StringDate.java
@@ -0,0 +1,19 @@
+package org.task;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class StringDate {
+	 
+		public static void main(String[] args)throws Exception {  
+		  
+			String sDate1="31/12/1998"; 
+ 
+		    Date date1=new SimpleDateFormat("dd/MM/yyyy").parse(sDate1); 
+		   
+		   System.out.println(sDate1+"\t"+date1);
+		}  
+		}  
+
+
+
