61796ca51c3783cfefd208771470e9be16a726b6
Naresh <Naresh@Naresh23>
2023-04-14 15:45:42 +0530

    Pattern

--- Project/src/org/task/Pattern.java
+++ Project/src/org/task/Pattern.java
@@ -7,15 +7,17 @@ public class Pattern {
 	 
 	int i, j;   
 	 
-	for(i=0; i<6; i++)   	{   
+	for(i=0; i<6; i++)  {   
 	
 		for(j=0; j<=i; j++)   {   
 	  
 	System.out.print("* ");   
+	
 	}   
 	
 	System.out.println();   
-	}   
+	
+	}
 	}   
 	}  
 
